8a1709c1aecf4c530fd77888b1ebacde00278b80
liseversmissenpxl <lise.versmissen@student.pxl.be>
2022-11-24 12:14:59 +0100

    git commit -m

--- navbar.js
+++ navbar.js
@@ -1,8 +1,11 @@
 function togglemenu() {
-    var x = document.getElementById("menubar");
-    if (x.style.display === "block") {
-        x.style.display = "none";
-    } else {
-        x.style.display = "block";
-    }
+    var menubar = document.getElementById("menubar");
+        if (menubar.style.maxHeight === "0px")
+        {
+            menubar.style.maxHeight = "180px"
+        }
+        else
+        {
+            menubar.style.maxHeight = "0px"
+        }
 }
